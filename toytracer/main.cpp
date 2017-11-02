7f9aaba5b63df46d2980d83d14415f7d9063fd84
Adam Fish <adamleighfish@gmail.com>
2017-11-02 14:36:58 -0400

    Initial Commit

---
+++ toytracer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  toytracer
+//
+//  Created by Adam Fish on 11/2/17.
+//  Copyright © 2017 Adam Fish. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
